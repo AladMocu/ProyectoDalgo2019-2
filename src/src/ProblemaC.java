2d6d72a40837ca07992ce0dce2a376f16efedec1
Albert Adolfo Molano Cubillos <aa.molano@uniandes.edu.co>
2019-11-30 16:16:59 -0500

    Small change C

--- src/src/ProblemaC.java
+++ src/src/ProblemaC.java
@@ -3,31 +3,28 @@ import java.util.Scanner;
 
 public class ProblemaC {
 
-    private  Point[] points;
+    private Point[] points;
 
-    public ProblemaC()
-    {
+    public ProblemaC() {
         Scanner sc = new Scanner(System.in);
         String work;
-        int dominio,lados,px,py;
-        while (sc.hasNext())
-        {
-            work=sc.nextLine();
-            if(work.equals("0 0 0 0"))
-            {
+        int dominio, lados, px, py;
+        while (sc.hasNext()) {
+            work = sc.nextLine();
+            if (work.equals("0 0 0 0")) {
                 break;
             }
-            String[] current= work.split(" ");
-            Point tested= new Point(current[2],current[3]);
-            int points= Integer.parseInt(current[1]);
-            String[] coords= sc.nextLine().split(" ");
-            String x,y;
-            for (int i = 0; i < points ;) {
-                x=coords[i];
+            String[] current = work.split(" ");
+            Point tested = new Point(current[2], current[3]);
+            int points = Integer.parseInt(current[1]);
+            String[] coords = sc.nextLine().split(" ");
+            String x, y;
+            for (int i = 0; i < points; ) {
+                x = coords[i];
                 i++;
-                y=coords[i];
+                y = coords[i];
                 i++;
-                Point p= new Point(x,y);
+                Point p = new Point(x, y);
             }
 
         }
@@ -45,18 +42,20 @@ public class ProblemaC {
         int j;
         boolean result = false;
         for (i = 0, j = points.length - 1; i < points.length; j = i++) {
-            if ((points[i].y > test.y) != (points[j].y > test.y) &&  (test.x < (points[j].x - points[i].x) * (test.y - points[i].y) / (points[j].y-points[i].y) + points[i].x)) {
+            if ((points[i].y > test.y) != (points[j].y > test.y) && (test.x < (points[j].x - points[i].x) * (test.y - points[i].y) / (points[j].y - points[i].y) + points[i].x)) {
                 result = !result;
             }
         }
-        if(result)
-        {
-            System.out.println(1);
-        }
-        else
-        {
+        if (result) {
+            inline(test);
+        } else {
             System.out.println(-1);
         }
+    }
+
+    public void inline(Point test)
+    {
+        System.out.println(1);
     }
 
 
